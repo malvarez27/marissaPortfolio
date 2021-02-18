ec0bed5eeb007da3b8795efcdf9262a6ae2c1713
Marissa <malar52@lion.lmu.edu>
2021-02-18 10:04:15 -0800

    updated descriptions

--- src/resume.js
+++ src/resume.js
@@ -57,7 +57,7 @@ const resume = {
             "title": "LOOIE",
             "summary": "Logical Organizations of Integrated Environments is an Internally developed web application for" +
             "the Factory of the Future (FoF) initiative at Northrop Grumman. The LOOIE web application tracks and manages tools, support equipment, and material movement from receipt to deployment." +
-            " Click the link to learn more about my role on this project! *Portfolio was cleared for public release* " ,
+            " Click the link to learn more about my role on this project!" ,
             "url": "https://github.com/malvarez27/marissaPortfolio/blob/master/public/images/Looie.pdf",
             "img": "/images/Homepage.png"
         },
